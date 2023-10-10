64272eb9b88709b7ae8d914f915856dd05b55c6f
Cem Yurtdas <cem.cem.strix@gmail.com>
2023-10-10 20:08:37 +0300

    Metrekare hesabı yapan basit C++ programı.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ int main() {
     cout << "Lütfen odanın ikinci kenarının ölçüsünü Metre olarak girin : " ;
     cin >> room_B;
     cout << "Odanın alanı " << room_A * room_B << " metrekaredir. ";
-
     return 0;
 
 
